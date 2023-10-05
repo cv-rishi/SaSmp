ea3596f406319022d3578b325e1fe370b6a2d547
CV Rishi <plsbetmax69420@gmail.com>
2023-10-06 01:46:43 +0530

    e

--- src/Preprocess.py
+++ src/Preprocess.py
@@ -5,9 +5,10 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
+nltk.download("stopwords")
+nltk.download("punkt")
+nltk.download("wordnet")
+
 
 # Function to preprocess text
 def preprocess_text(text):
@@ -15,16 +16,16 @@ def preprocess_text(text):
     text = text.lower()
 
     # Remove URLs (assuming URLs are prefixed with "http" or "https")
-    text = re.sub(r'http\S+', '', text)
+    text = re.sub(r"http\S+", "", text)
 
     # Remove special characters, symbols, and punctuation
-    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r"[^\w\s]", "", text)
 
     # Tokenization
     tokens = word_tokenize(text)
 
     # Remove stopwords
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words("english"))
     tokens = [word for word in tokens if word not in stop_words]
 
     # Lemmatization
@@ -32,24 +33,25 @@ def preprocess_text(text):
     tokens = [lemmatizer.lemmatize(word) for word in tokens]
 
     # Join tokens back into a string
-    cleaned_text = ' '.join(tokens)
+    cleaned_text = " ".join(tokens)
 
     return cleaned_text
+
 
 # Read data from a CSV file
 def preprocess_csv(input_csv, output_csv):
     df = pd.read_csv(input_csv)
 
     # Apply preprocessing to the 'text' column
-    df['cleaned_text'] = df['text'].apply(preprocess_text)
+    df["cleaned_text"] = df["text"].apply(preprocess_text)
 
     # Save the cleaned DataFrame to a new CSV file
     df.to_csv(output_csv, index=False)
 
+
 # Example usage
 if __name__ == "__main__":
-    input_csv = "input_data.csv"   # Replace with your CSV file
-    output_csv = "cleaned_data.csv"   # Replace with your desired output CSV file
+    input_csv = "input_data.csv"  # Replace with your CSV file
+    output_csv = "cleaned_data.csv"  # Replace with your desired output CSV file
 
     preprocess_csv(input_csv, output_csv)
-
